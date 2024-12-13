bc730934fc1aed0044ac763a8f5329a880f8674f
ersinisgor <ersinisgor@hotmail.com>
2024-12-13 10:40:58 +0300

    Refactor Home screen to use HomeHeader; Add HomeHeader component to encapsulate header UI

---
+++ src/components/HomeHeader.jsx
@@ -0,0 +1,120 @@
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { COLORS, PADDING_SM } from "../utils/constants";
+
+const HomeHeader = () => {
+  return (
+    <>
+      <View style={styles.profileContainer}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../../assets/images/profile.jpg")}
+            style={styles.profileImage}
+          />
+          <View style={styles.badge}></View>
+          <View style={styles.badge_2}></View>
+        </View>
+
+        <Text>
+          👋 Hello <Text style={styles.name}>John</Text>
+        </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchBar}>
+          <TextInput
+            style={styles.searchText}
+            placeholder="Search for hotels"
+          />
+          <Pressable onPress={() => {}}>
+            <MaterialIcons name="search" size={24} />
+          </Pressable>
+        </View>
+        <View style={styles.filterIcon}>
+          <TouchableOpacity>
+            <MaterialIcons
+              name="filter-list"
+              size={24}
+              color={COLORS.primary}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default HomeHeader;
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  imageContainer: {
+    position: "relative",
+    width: 50,
+    marginRight: PADDING_SM,
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  badge: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 14,
+    height: 14,
+    backgroundColor: "green",
+    borderRadius: 7,
+    zIndex: 1,
+  },
+  badge_2: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 16,
+    height: 16,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+  },
+  name: {
+    fontFamily: "Poppins-SemiBold",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: PADDING_SM,
+  },
+  searchBar: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 8,
+    backgroundColor: COLORS.inActiveBackground,
+    borderRadius: 10,
+    marginRight: PADDING_SM,
+  },
+  searchText: {
+    flex: 1,
+    color: COLORS.inActiveFont,
+    fontSize: 14,
+    fontFamily: "Poppins-Medium",
+    paddingHorizontal: 8,
+  },
+  filterIcon: {
+    padding: 8,
+    borderWidth: 1,
+    borderColor: COLORS.inActiveLine,
+  },
+});
--- src/screens/tabs/Home.jsx
+++ src/screens/tabs/Home.jsx
@@ -1,13 +1,4 @@
-import {
-  Image,
-  Pressable,
-  StyleSheet,
-  Switch,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { StyleSheet, Switch, Text, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
@@ -19,6 +10,7 @@ import { COLORS, PADDING, PADDING_SM, WIDTH } from "../../utils/constants";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../../../firebase";
 import Slider from "../../components/Slider";
+import HomeHeader from "../../components/HomeHeader";
 
 const Home = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
@@ -57,40 +49,7 @@ const Home = () => {
         <View style={styles.content}>
           {/* Header */}
           <View style={styles.headerContainer}>
-            <View style={styles.profileContainer}>
-              <View style={styles.imageContainer}>
-                <Image
-                  source={require("../../../assets/images/profile.jpg")}
-                  style={styles.profileImage}
-                />
-                <View style={styles.badge}></View>
-                <View style={styles.badge_2}></View>
-              </View>
-
-              <Text>
-                👋 Hello <Text style={styles.name}>John</Text>
-              </Text>
-            </View>
-            <View style={styles.searchContainer}>
-              <View style={styles.searchBar}>
-                <TextInput
-                  style={styles.searchText}
-                  placeholder="Search for hotels"
-                />
-                <Pressable onPress={() => {}}>
-                  <MaterialIcons name="search" size={24} />
-                </Pressable>
-              </View>
-              <View style={styles.filterIcon}>
-                <TouchableOpacity>
-                  <MaterialIcons
-                    name="filter-list"
-                    size={24}
-                    color={COLORS.primary}
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
+            <HomeHeader />
           </View>
 
           {/* Main Content */}
@@ -175,68 +134,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginVertical: PADDING,
   },
-  profileContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  imageContainer: {
-    position: "relative",
-    width: 50,
-    marginRight: PADDING_SM,
-  },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-  },
-  badge: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    width: 14,
-    height: 14,
-    backgroundColor: "green",
-    borderRadius: 7,
-    zIndex: 1,
-  },
-  badge_2: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    width: 16,
-    height: 16,
-    backgroundColor: "#fff",
-    borderRadius: 8,
-  },
-  name: {
-    fontFamily: "Poppins-SemiBold",
-  },
-  searchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: PADDING_SM,
-  },
-  searchBar: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 8,
-    backgroundColor: COLORS.inActiveBackground,
-    borderRadius: 10,
-    marginRight: PADDING_SM,
-  },
-  searchText: {
-    flex: 1,
-    color: COLORS.inActiveFont,
-    fontSize: 14,
-    fontFamily: "Poppins-Medium",
-    paddingHorizontal: 8,
-  },
-  filterIcon: {
-    padding: 8,
-    borderWidth: 1,
-    borderColor: COLORS.inActiveLine,
-  },
+
   // Main Content
   mainContent: {
     flex: 1,
