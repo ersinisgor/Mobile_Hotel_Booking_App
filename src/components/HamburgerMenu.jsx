584f71ff2daf3bd4cc3b79018015e0408660429b
ersinisgor <ersinisgor@hotmail.com>
2024-12-13 12:11:11 +0300

    Add expo-blur package and implement blur effect in HamburgerMenu component; enhance menu overlay appearance

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,6 +1,13 @@
-import { StyleSheet, Switch, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { COLORS, PADDING, WIDTH } from "../utils/constants";
+import {
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+  Pressable,
+} from "react-native";
+import { BlurView } from "expo-blur";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -8,6 +15,7 @@ import {
   MaterialCommunityIcons,
   Ionicons,
 } from "@expo/vector-icons";
+import { COLORS, PADDING_SM, WIDTH } from "../utils/constants";
 
 const HamburgerMenu = ({
   toggleMenu,
@@ -18,40 +26,54 @@ const HamburgerMenu = ({
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={styles.menu} edges={["top", "left", "right"]}>
-      <View style={styles.menuView}>
-        <TouchableOpacity style={styles.closeButton} onPress={toggleMenu}>
-          <MaterialIcons name="close" size={24} color="#fff" />
-        </TouchableOpacity>
+    <View style={StyleSheet.absoluteFill}>
+      {/* Background overlay with blur */}
+      <BlurView intensity={50} style={styles.overlay}>
+        <Pressable style={StyleSheet.absoluteFill} onPress={toggleMenu} />
+      </BlurView>
 
-        <TouchableOpacity
-          style={styles.menuItem}
-          onPress={() => {
-            toggleMenu();
-            navigation.navigate("EditProfile");
-          }}
-        >
-          <MaterialCommunityIcons name="account-edit" size={24} color="#fff" />
-          <Text style={styles.menuItemText}>Edit Profile</Text>
-        </TouchableOpacity>
+      {/* Menu */}
+      <View style={styles.menu}>
+        <SafeAreaView>
+          <TouchableOpacity style={styles.closeButton} onPress={toggleMenu}>
+            <MaterialIcons name="close" size={24} color="#fff" />
+          </TouchableOpacity>
+        </SafeAreaView>
 
-        <View style={styles.menuItem}>
-          <Ionicons name="moon" size={21} color="#fff" />
-          <Text style={styles.menuItemText}>Dark Mode</Text>
-          <Switch
-            value={isDarkMode}
-            onValueChange={setIsDarkMode}
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
-          />
+        <View style={styles.menuView}>
+          <TouchableOpacity
+            style={styles.menuItem}
+            onPress={() => {
+              toggleMenu();
+              navigation.navigate("EditProfile");
+            }}
+          >
+            <MaterialCommunityIcons
+              name="account-edit"
+              size={24}
+              color="#fff"
+            />
+            <Text style={styles.menuItemText}>Edit Profile</Text>
+          </TouchableOpacity>
+
+          <View style={styles.menuItem}>
+            <Ionicons name="moon" size={21} color="#fff" />
+            <Text style={styles.menuItemText}>Dark Mode</Text>
+            <Switch
+              value={isDarkMode}
+              onValueChange={setIsDarkMode}
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+            />
+          </View>
+
+          <TouchableOpacity style={styles.menuItem} onPress={handleLogout}>
+            <MaterialIcons name="logout" size={24} color="#fff" />
+            <Text style={styles.menuItemText}>Logout</Text>
+          </TouchableOpacity>
         </View>
-
-        <TouchableOpacity style={styles.menuItem} onPress={handleLogout}>
-          <MaterialIcons name="logout" size={24} color="#fff" />
-          <Text style={styles.menuItemText}>Logout</Text>
-        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -63,17 +85,21 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     bottom: 0,
-    width: WIDTH / 2,
-
+    flex: 1,
     backgroundColor: COLORS.primary,
     zIndex: 2,
+    padding: PADDING_SM,
+    width: WIDTH / 2,
   },
-  menuView: {
-    padding: PADDING,
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1,
   },
   closeButton: {
     alignSelf: "flex-end",
-    marginBottom: WIDTH / 4,
+  },
+  menuView: {
+    marginTop: WIDTH / 4,
   },
   menuItem: {
     flexDirection: "row",
@@ -81,7 +107,7 @@ const styles = StyleSheet.create({
     marginBottom: 24,
   },
   menuItemText: {
-    marginLeft: 16,
+    marginLeft: PADDING_SM,
     color: "#fff",
     fontSize: 16,
   },
